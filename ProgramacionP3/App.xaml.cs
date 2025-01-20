3771221bd80605d9daba441a41af5507cd01363a
Wikiniki15 <wiki.niki.olalla@gmail.com>
2025-01-20 16:20:43 -0500

    Add project files.

---
+++ ProgramacionP3/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ProgramacionP3
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
